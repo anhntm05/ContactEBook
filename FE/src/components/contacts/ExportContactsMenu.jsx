da062e1756b2513086c2a62b19c9623e6b6ce1d7
MinhAnhNek <anhntmhe194390@gmail.com>
2026-03-09 16:06:49 +0700

    [FE] feat: add export files
    - choose type of file: .xsls, .csv, .vcf

--- FE/src/components/contacts/ExportContactsMenu.jsx
+++ FE/src/components/contacts/ExportContactsMenu.jsx
@@ -59,9 +59,9 @@ const ExportContactsMenu = ({
               type="button"
               onClick={() => handleExport(onExportVCard)}
               className="w-full text-left px-4 py-2 text-sm hover:bg-gray-50"
-              title="Export selected contacts to vCard (.vcf) for phone/contact apps."
+              title="Export selected contacts to vCard (.vcf), which can be dragged directly into Apple Contacts."
             >
-              Export to vCard (.vcf)
+              Export to Apple Contacts (.vcf)
             </button>
           )}
         </div>
