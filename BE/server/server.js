caa07b83726ceab6488318f3fee0ee893fe2b0f8
MinhAnhNek <anhntmhe194390@gmail.com>
2026-03-13 22:26:57 +0700

    [BE] chore: ...

--- BE/server/server.js
+++ BE/server/server.js
@@ -36,6 +36,6 @@ const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running at link: http://localhost:${PORT}`);
   });
 });
